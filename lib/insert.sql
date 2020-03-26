fe50b75000101eb2b458192a6f528f951d9d85e3
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-03-26 01:49:59 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 20, "M", "dark brown", "Calm", 0);
+    
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, "Rowdy", 10, "M", "tan", "Goofy", 1);
+  
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, "Tabitha", 6, "F", "dark brown", "Timid", 1);
+  
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, "Sergeant Brown", 3, "M", "black","Cranky", 0);
+  
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, "Melissa", 13, "F", "blonde", "Nice", 1);
+  
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, "Grinch", 2, "M", "black", "Aggressive", 1);
+  
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, "Wendy", 6, "F", "dark brown", "Blonde", 1);
+  
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, null , 20, "M", "tan", "Aggressive", 0);
